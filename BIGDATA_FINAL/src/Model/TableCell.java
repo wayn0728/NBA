9368623a5ae3712882a09a1fbe72535910bce81d
Tony <Tony@10.0.0.9>
2013-10-27 11:00:28 -0400

    cq

---
+++ BIGDATA_FINAL/src/Model/TableCell.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class TableCell {
+	public String time;
+	public String awayRecord;
+	public String awayEvent;
+	public String score;
+	public String homeRecord;
+	public String homeEvent;
+	public int num;
+	public int n;
+	public TableCell(){
+		time="";
+		awayRecord="";
+		awayEvent="";
+		score="";
+		homeRecord="";
+		homeEvent="";
+	}
+
+}
+
+
