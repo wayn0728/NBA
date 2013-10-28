377e5f44ab9084c3710e9cb7cfdfd8a9bf1ed7a7
Tony <Tony@10.0.0.9>
2013-10-28 00:08:48 -0400

    NBA attack defend average time

---
+++ BIGDATA_FINAL/src/nba_datamining/TableCell.java
@@ -0,0 +1,23 @@
+package nba_datamining;
+
+public class TableCell {
+	public String time;
+	public String awayRecord;
+	public String awayEvent;
+	public String score;
+	public String homeRecord;
+	public String homeEvent;
+	public int num;
+	public int n;
+	public TableCell(){
+		time="";
+		awayRecord="";
+		awayEvent="";
+		score="";
+		homeRecord="";
+		homeEvent="";
+	}
+
+}
+
+
